ea44e4d57fc857e539aea1189efdea52525b52a0
Jessica Schmidt <jessyka_sl@hotmail.com>
2025-08-19 21:32:42 -0300

    Criação do servidor express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+import express from 'express';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Definir rotas
+
+const porta = process.env.PORT;
+app.listen(porta, () => {
+    console.log(`O servidor esta rodando na porta ${porta}`);
+});
+
