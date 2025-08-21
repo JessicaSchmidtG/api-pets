0a63f1ae4a0970eecee7660d9f41c65ad0836bc9
Jessica Schmidt <jessyka_sl@hotmail.com>
2025-08-21 19:15:15 -0300

    Rotas para obter, atualizar e excluir pet pelo id

--- src/index.js
+++ src/index.js
@@ -61,6 +61,101 @@ app.post("/pets", (req, res) => {
     }
 });
 
+// Obter um pet pelo ID (GET/pets/:id) 
+app.get("/pets/:id",(req,res) => {
+    try {
+        //entrada
+        const { id } = req.params;
+        //processamento
+        const pet = pets.find(item => item.id === id);
+        if(!pet){
+            return res.status(404).send({
+                ok: false,
+                mensagem: "Pet não encontrado"
+            });
+        } 
+        //saida
+        res.status(200).send({
+            ok: true,
+            mensagem: "Pet encontrado com sucesso",
+            dados: pet
+        });
+
+    } catch (error) {
+        res.status(500).send({
+            ok: false,
+            mensagem: error.toString()
+        });
+    }
+});
+
+//Atualizar um pet existente pelo ID (PUT/pets/:id)
+app.put("/pets/:id", (req,res) => {
+    try {
+        //entrada
+        const { id } = req.params;
+        const { nome, raca, idade, nomeTutor } = req.body;
+
+        //processamento
+        const pet = pets.find(item => item.id === id);
+        if(!pet){
+            return res.status(404).send({
+                ok: false,
+                mensagem: "Pet não encontrado"
+            });
+        } 
+
+        pet.nome = nome;
+        pet.raca = raca;
+        pet.idade = idade;
+        pet.nomeTutor = nomeTutor;
+
+        //saida
+        res.status(200).send({
+            ok: true,
+            mensagem: "Pet atualizado com sucesso",
+            dados: pets
+        });
+    } catch (error) {
+        res.status(500).send({
+            ok: false,
+            mensagem: error.toString()
+        });
+    }
+});
+
+// Excluir um pet existente (DELETE /pets/:id)
+
+app.delete("/pets/:id", (req, res) => {
+    try {
+        //entrada
+        const { id } = req.params;
+        //processamento
+        const petIndex = pets.findIndex(item => item.id === id);
+        if(petIndex < 0){
+            return res.status(404).send({
+                ok: false,
+                mensagem: "Pet não encontrado"
+            });
+        } 
+
+        pets.splice(petIndex, 1);
+
+        //saida
+        res.status(200).send({
+            ok: true,
+            mensagem: "Pet excluido com sucesso!",
+            dados: pets
+        })
+        
+    } catch (error) {
+        res.status(500).send({
+            ok: false,
+            mensagem: error.toString()
+        });
+    }
+});
+
 const porta = process.env.PORT;
 app.listen(porta, () => {
     console.log(`O servidor esta rodando na porta ${porta}`);
